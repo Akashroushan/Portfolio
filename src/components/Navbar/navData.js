1f6cc318bd9fdde8407cc7f2db2e27732737be75
Akashroushan <66554765+Akashroushan@users.noreply.github.com>
2021-05-31 07:46:12 +0530

    Add files via upload

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,38 @@
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/',
+    hash: '#project-1',
+  },
+  //{
+ //   label: 'Articles',
+ //  pathname: '/articles',
+ // },
+  {
+    label: 'About',
+    pathname: '/',
+    hash: '#about',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/aakashroushann',
+    icon: 'twitter',
+  },
+  {
+    label: 'linkedin',
+    url: 'https://www.linkedin.com/in/akash-roushan-65530614b/',
+    icon: 'linkedin',
+  },
+  {
+    label: 'Github',
+    url: 'https://github.com/Akashroushan',
+    icon: 'github',
+  },
+];
